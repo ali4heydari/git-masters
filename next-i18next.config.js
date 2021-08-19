2147bd7775de8a83389c53029503c7c24dcd081f
Ali Heydari <ali4heydari@gmail.com>
2021-08-19 22:10:51 +0200

    fix(*): 🐛 fix build error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,9 @@
-const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
 const path = require("path");
-
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'tr'],
+    defaultLocale: "en",
+    locales: ["en", "tr"],
     localePath: path.resolve("./public/locales"),
-    localeSubpaths,
   },
 };
