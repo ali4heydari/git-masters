89115aeeb8f3dfeb095c867568a1794746d18e00
Ali Heydari <ali4heydari@gmail.com>
2021-08-19 22:32:50 +0200

    refactor(components): 🧹 refactor main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,18 +4,34 @@ import { Button } from "@components";
 
 export const Main: React.FC = () => {
   return (
-    <div className="text-center font-light py-5 bg-gray-700">
-      <div className="container mx-auto">
-        <h1 className="text-white text-8xl mb-2">superplate</h1>
-        <p className="text-lg text-white mb-3">
-          The frontend boilerplate with superpowers!
-        </p>
-        <Button type="button">
-          <a href="https://pankod.github.io/superplate/" target="_blank">
-            Docs
-          </a>
-        </Button>
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <img
+          className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          alt="hero"
+          src="https://dummyimage.com/720x600"
+        />
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            Microdosing synth tattooed vexillologist
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
+            tousled. Chambray dreamcatcher trust fund, kitsch vice godard
+            disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.
+            Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan
+            photo booth af fingerstache pitchfork.
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Button
+            </button>
+            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+              Button
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
