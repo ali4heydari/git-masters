30855c61a6fbc09f3b0069d400d52c2f4678dbc2
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 16:19:19 +0430

    refactor(*): specify typings for nextConfig in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withPlugins = require("next-compose-plugins");
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
-module.exports = withPlugins([], {
+// If such a type existed...
+/** @type { import("next").NextConfig } */
+const nextConfig = {
   i18n,
-});
+};
+
+module.exports = withPlugins([], nextConfig);
