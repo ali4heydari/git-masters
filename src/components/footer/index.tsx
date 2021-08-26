49148f7c0b373876b227a3b60eed1f419b08302f
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 16:21:33 +0430

    feat(components): update site logo in header and footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,6 +7,7 @@ import {
   SITE_AUTHOR_TWITTER_ID,
   SITE_NAME,
 } from "../../lib/constants";
+import { GitLogo } from "@components/icons";
 
 export const Footer: React.FC = () => {
   return (
@@ -16,18 +17,7 @@ export const Footer: React.FC = () => {
           href={PUBLIC_URL}
           className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-          </svg>
+          <GitLogo className="w-10 h-10 text-white p-0 rounded-full" />
           <span className="ml-3 text-xl">{SITE_NAME}</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
