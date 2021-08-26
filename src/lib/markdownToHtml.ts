bfa62fac0ce1e12793380343fc09d4012c1b9eee
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 22:36:08 +0430

    feat(lib): add remarkPrism plugins

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -3,6 +3,21 @@ import html from "remark-html";
 import remarkPrism from "remark-prism";
 
 export default async function markdownToHtml(markdown: any) {
-  const result = await remark().use(html).use(remarkPrism).process(markdown);
+  const result = await remark()
+    .use(html)
+    .use(remarkPrism, {
+      plugins: [
+        "autolinker",
+        "command-line",
+        "data-uri-highlight",
+        "diff-highlight",
+        "inline-color",
+        "keep-markup",
+        "line-numbers",
+        "show-invisibles",
+        "treeview",
+      ],
+    })
+    .process(markdown);
   return result.toString();
 }
