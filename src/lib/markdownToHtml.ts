72e08fcb41136ee1f24002a689050640925c3cc0
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 22:20:38 +0430

    feat(lib): use synatx highliting with remark prism

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import { remark } from "remark";
 import html from "remark-html";
+import remarkPrism from "remark-prism";
 
 export default async function markdownToHtml(markdown: any) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(html).use(remarkPrism).process(markdown);
   return result.toString();
 }
