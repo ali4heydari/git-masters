99cf4fac3a512d526e378cc62989ec4aef8e4131
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 22:38:09 +0430

    feat(components): add meta component to layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container, Footer, Header } from "@components";
+import { Meta } from "@components/meta";
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <Container>
+      <Meta />
       <Header />
       {children}
       <Footer />
