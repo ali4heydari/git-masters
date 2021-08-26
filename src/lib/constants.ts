e47945dd076b78c259a2dabd842ec7f831128c19
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 15:08:34 +0430

    refactor(lib): add more constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,9 @@
 export const HOME_OG_IMAGE_URL = "";
 export const SITE_DESCRIPTION = "";
 export const SITE_NAME = "Git workshop";
+export const SITE_AUTHOR = "Ali Heydari";
+export const SITE_AUTHOR_TWITTER_ID = "Ali4Heydari";
+export const SITE_AUTHOR_FACEBOOK_ID = "Ali4Heydari";
+export const SITE_AUTHOR_INSTAGRAM_ID = "Ali4Heydari";
+export const SITE_AUTHOR_LINKEDIN_ID = "Ali4Heydari";
+export const PUBLIC_URL = "/";
