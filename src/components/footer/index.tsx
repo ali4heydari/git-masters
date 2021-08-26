d622f1119255605ceb9558f1dbfe57c3d2508185
Ali Heydari <ali4heydari@gmail.com>
2021-08-26 15:09:04 +0430

    refactor(components): 🦶 refactor footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import {
-  PankodIcon,
-  GithubIcon,
-  TwitterIcon,
-  YoutubeIcon,
-  LinkedinIcon,
-} from "@components/icons";
+  PUBLIC_URL,
+  SITE_AUTHOR_FACEBOOK_ID,
+  SITE_AUTHOR_INSTAGRAM_ID,
+  SITE_AUTHOR_LINKEDIN_ID,
+  SITE_AUTHOR_TWITTER_ID,
+  SITE_NAME,
+} from "../../lib/constants";
 
 export const Footer: React.FC = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <a
+          href={PUBLIC_URL}
+          className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -24,21 +28,24 @@ export const Footer: React.FC = () => {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
           </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">{SITE_NAME}</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2020 Tailblocks —
+          © {new Date().getFullYear()} {SITE_NAME} —
           <a
-            href="https://twitter.com/knyttneve"
+            href={`https://twitter.com/${SITE_AUTHOR_TWITTER_ID}`}
             className="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
           >
-            @knyttneve
+            @{SITE_AUTHOR_TWITTER_ID}
           </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-500">
+          <a
+            href={`https://facebook.com/${SITE_AUTHOR_FACEBOOK_ID}`}
+            className="text-gray-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -50,7 +57,10 @@ export const Footer: React.FC = () => {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a
+            href={`https://twitter.com/${SITE_AUTHOR_TWITTER_ID}`}
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -62,7 +72,10 @@ export const Footer: React.FC = () => {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a
+            href={`https://instagram.com/${SITE_AUTHOR_INSTAGRAM_ID}`}
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="none"
               stroke="currentColor"
@@ -76,7 +89,10 @@ export const Footer: React.FC = () => {
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
             </svg>
           </a>
-          <a className="ml-3 text-gray-500">
+          <a
+            href={`https://linkedin.com/${SITE_AUTHOR_LINKEDIN_ID}`}
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="currentColor"
               stroke="currentColor"
