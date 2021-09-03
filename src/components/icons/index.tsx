49f6020495237e8fc0ba38615a7ecf18c1d45eb8
Ali Heydari <ali4heydari@gmail.com>
2021-09-03 10:24:44 +0430

    feat(icons): add icons for syllabus section

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,9 +1,15 @@
+export { default as ClockIcon } from "./ClockIcon";
+export { default as CollaborationIcon } from "./CollaborationIcon";
 export { default as GitLogo } from "./GitLogo";
 export { default as GithubIcon } from "./GithubIcon";
 export { default as LinkedinIcon } from "./LinkedinIcon";
 export { default as MoonIcon } from "./MoonIcon";
 export { default as NextjsIcon } from "./NextjsIcon";
 export { default as PankodIcon } from "./PankodIcon";
+export { default as RewriteTextIcon } from "./RewriteTextIcon";
+export { default as SnapshotIcon } from "./SnapshotIcon";
+export { default as StartIcon } from "./StartIcon";
 export { default as SunIcon } from "./SunIcon";
+export { default as ToolsIcon } from "./ToolsIcon";
 export { default as TwitterIcon } from "./TwitterIcon";
 export { default as YoutubeIcon } from "./YoutubeIcon";
