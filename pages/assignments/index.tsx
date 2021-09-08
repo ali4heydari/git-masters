3320c1a803826ef8445b5c1a2fb77421ce5fdf94
Ali Heydari <ali4heydari@gmail.com>
2021-09-08 19:22:09 +0430

    refactor(pages): refactor assigmnent page to use from markdowns

--- pages/assignments/index.tsx
+++ pages/assignments/index.tsx
@@ -1,33 +1,11 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Layout } from "@components";
+import { Assignment, getAllAssignments } from "@lib/api";
+import Link from "next/link";
 
-const Assignments: React.FC = (props) => {
-  const assignments = useMemo(
-    () => [
-      {
-        title: "Lorem ipsum dolor sit amet",
-        description:
-          "consectetur adipisicing elit. Ab amet at cupiditate delectus dignissimos id ipsam, laboriosam quasi repudiandae tempora. Animi ea, illo in ipsa odit omnis quis quod veniam",
-      },
-      {
-        title: "Lorem ipsum dolor sit amet",
-        description:
-          "consectetur adipisicing elit. Ab amet at cupiditate delectus dignissimos id ipsam, laboriosam quasi repudiandae tempora. Animi ea, illo in ipsa odit omnis quis quod veniam",
-      },
-      {
-        title: "Lorem ipsum dolor sit amet",
-        description:
-          "consectetur adipisicing elit. Ab amet at cupiditate delectus dignissimos id ipsam, laboriosam quasi repudiandae tempora. Animi ea, illo in ipsa odit omnis quis quod veniam",
-      },
-      {
-        title: "Lorem ipsum dolor sit amet",
-        description:
-          "consectetur adipisicing elit. Ab amet at cupiditate delectus dignissimos id ipsam, laboriosam quasi repudiandae tempora. Animi ea, illo in ipsa odit omnis quis quod veniam",
-      },
-    ],
-    []
-  );
-
+const Assignments: React.FC<{
+  allAssignments: Assignment[];
+}> = ({ allAssignments }) => {
   return (
     <Layout>
       <section className="text-gray-600 body-font">
@@ -36,26 +14,80 @@ const Assignments: React.FC = (props) => {
             Assignments
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Banh mi cornhole echo park skateboard authentic crucifix neutra
-            tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon
-            twee
+            This assignments helps you to practice and having deep understanding
+            of concepts.
           </p>
         </div>
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
-          {assignments.map((it, idx) => (
+          {allAssignments.map((it, idx) => (
             <div
-              key={it.title}
+              key={`${it.title}-${it?.dueDate}`}
               className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto"
             >
-              <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
-                {idx + 1}
-              </div>
-              <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                  <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
-                    {it.title}
-                  </h2>
-                  <p className="leading-relaxed">{it.description}</p>
+              <div>
+                {it.tags?.map((tag) => (
+                  <span
+                    key={tag}
+                    className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest mx-1"
+                  >
+                    {tag}
+                  </span>
+                ))}
+
+                <h2 className="sm:text-2xl text-xl title-font font-medium text-gray-900 mt-4 mb-4">
+                  {it.title}
+                </h2>
+                <p className="leading-relaxed mb-8">{it.excerpt}</p>
+                <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
+                  <Link
+                    as={`/assignments/${it.slug}`}
+                    href="/assignments/[slug]"
+                  >
+                    <a className="text-indigo-500 inline-flex items-center">
+                      More
+                      <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                      </svg>
+                    </a>
+                  </Link>
+                  <span className="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                      <circle cx={12} cy={12} r={3} />
+                    </svg>
+                    {it.dueDate}
+                  </span>
+                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                    </svg>
+                    {it.solution}
+                  </span>
                 </div>
               </div>
             </div>
@@ -65,5 +97,23 @@ const Assignments: React.FC = (props) => {
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const allAssignments = getAllAssignments([
+    "title",
+    "documentation",
+    "dueDate",
+    "releaseDate",
+    "attachment",
+    "slug",
+    "solution",
+    "tags",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allAssignments },
+  };
+}
 
 export default Assignments;
