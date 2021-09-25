8bcf7acc68ea252afe0463ade6e27e8b3a526a2d
Ali Heydari <ali4heydari@gmail.com>
2021-09-26 00:09:19 +0330

    refactor(components): update hero section

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,34 +1,36 @@
 import React from "react";
-
-import { Button } from "@components";
+import Link from "next/link";
 
 export const Main: React.FC = () => {
   return (
     <section className="text-gray-600 body-font">
-      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+      <div className="container mx-auto flex px-5 py-2 items-center justify-center flex-col">
         <img
-          className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          className="lg:w-3/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="hero"
-          src="https://dummyimage.com/720x600"
+          src="/images/ali-heydari-high-resolution.jpg"
         />
+
         <div className="text-center lg:w-2/3 w-full">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Microdosing synth tattooed vexillologist
+            Now is the moment to learn Git
           </h1>
           <p className="mb-8 leading-relaxed">
-            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
-            tousled. Chambray dreamcatcher trust fund, kitsch vice godard
-            disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.
-            Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan
-            photo booth af fingerstache pitchfork.
+            Git is the world's most popular Version Control System (VCS), It
+            allows you to keep track of your development's history, reverse your
+            code in case of a failure, and collaborate successfully with others.
           </p>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
-            </button>
-            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-              Button
-            </button>
+            <Link href="/syllabus">
+              <a className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
+                Syllabus
+              </a>
+            </Link>
+            <Link href="/lectures">
+              <a className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Lectures
+              </a>
+            </Link>
           </div>
         </div>
       </div>
