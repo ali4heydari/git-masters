9115f06538b1bccceb0cfa744a39e349a4886197
Ali Heydari <ali4heydari@gmail.com>
2021-09-29 14:36:15 +0330

    fix(config): remote i18n from next config because of incopatibily with next export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 const withPlugins = require("next-compose-plugins");
 const { basePath } = require("./envs");
-const { i18n } = require("./next-i18next.config");
+// const { i18n } = require("./next-i18next.config");
 
 const isProd = process.env.NODE_ENV === "production";
 
 // If such a type existed...
 /** @type { import("next").NextConfig } */
 const nextConfig = {
-  i18n,
+  // i18n,
   basePath,
   assetPrefix: isProd ? `${basePath}/` : "",
 };
