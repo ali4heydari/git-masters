91e6c69dbdf5daf13198970e13ee7cff5426acf4
Ali Heydari <ali4heydari@gmail.com>
2021-09-29 19:53:18 +0330

    refactor(lib): 📄 change site description and og:image url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,10 @@
-export const HOME_OG_IMAGE_URL = "";
-export const SITE_DESCRIPTION = "";
+import { basePath } from "../../envs";
+
+export const HOME_OG_IMAGE_URL = `${basePath}/images/git-cover.jpeg`;
+export const SITE_DESCRIPTION = "Practical course to learn Git";
 export const SITE_NAME = "Git masters";
 export const SITE_AUTHOR = "Ali Heydari";
 export const SITE_AUTHOR_TWITTER_ID = "Ali4Heydari";
 export const SITE_AUTHOR_FACEBOOK_ID = "Ali4Heydari";
 export const SITE_AUTHOR_INSTAGRAM_ID = "Ali4Heydari";
 export const SITE_AUTHOR_LINKEDIN_ID = "Ali4Heydari";
-export const PUBLIC_URL = "/";
