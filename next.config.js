912e3bee9b020d7173f358123e775f61958ce172
Ali Heydari <ali4heydari@gmail.com>
2021-09-29 13:55:48 +0330

    chore(*): update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@ const withPlugins = require("next-compose-plugins");
 const { basePath } = require("./envs");
 const { i18n } = require("./next-i18next.config");
 
+const isProd = process.env.NODE_ENV === "production";
+
 // If such a type existed...
 /** @type { import("next").NextConfig } */
 const nextConfig = {
   i18n,
   basePath,
+  assetPrefix: isProd ? `${basePath}/` : "",
 };
 
 module.exports = withPlugins([], nextConfig);
