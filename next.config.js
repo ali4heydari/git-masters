2ae18898a1992f6d170fe96aeb08f270842d346e
Ali Heydari <ali4heydari@gmail.com>
2021-09-29 12:46:10 +0330

    chore(*): add envs file for putting env variables

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withPlugins = require("next-compose-plugins");
+const { basePath } = require("./envs");
 const { i18n } = require("./next-i18next.config");
 
 // If such a type existed...
 /** @type { import("next").NextConfig } */
 const nextConfig = {
   i18n,
+  basePath,
 };
 
 module.exports = withPlugins([], nextConfig);
