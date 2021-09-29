623a0b7f0e61779c3b4f0bd492d74ed3e04b6849
Ali Heydari <ali4heydari@gmail.com>
2021-09-29 19:19:27 +0330

    fix(components): fix logo link in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import {
-  PUBLIC_URL,
-  SITE_AUTHOR_FACEBOOK_ID,
-  SITE_AUTHOR_INSTAGRAM_ID,
   SITE_AUTHOR_LINKEDIN_ID,
   SITE_AUTHOR_TWITTER_ID,
   SITE_NAME,
-} from "../../lib/constants";
+} from "@lib/constants";
 import { GitLogo } from "@components/icons";
+import Link from "next/link";
 
 export const Footer: React.FC = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a
-          href={PUBLIC_URL}
-          className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
-        >
-          <GitLogo className="w-10 h-10 text-white p-0 rounded-full" />
-          <span className="ml-3 text-xl">{SITE_NAME}</span>
-        </a>
+        <Link href={"/"}>
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <GitLogo className="w-10 h-10 text-white p-0 rounded-full" />
+            <span className="ml-3 text-xl">{SITE_NAME}</span>
+          </a>
+        </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © {new Date().getFullYear()} {SITE_NAME} —
           <a
