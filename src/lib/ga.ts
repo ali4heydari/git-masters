1dc96656aad8a9abfa1880cb3e4ec466ba556424
Ali Heydari <ali4heydari@gmail.com>
2021-09-30 11:12:42 +0330

    fix(lib): bypass ts error

--- src/lib/ga.ts
+++ src/lib/ga.ts
@@ -2,12 +2,14 @@ import { googleAnalyticsCode } from "../../envs";
 
 // log the pageview with their URL
 export const pageview = (url: string) => {
+  // @ts-ignore
   window.gtag("config", googleAnalyticsCode, {
     page_path: url,
   });
 };
 
 // log specific events happening.
+// @ts-ignore
 export const event = ({ action, params }) => {
   window.gtag("event", action, params);
 };
