0e018ae397b58776f35d268043672639fb8409a4
Ali Heydari <ali4heydari@gmail.com>
2021-10-28 06:40:19 +0330

    feat(*): 💊 add sentry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 const withPlugins = require("next-compose-plugins");
+const { withSentryConfig } = require("@sentry/nextjs");
 const { basePath, domain } = require("./envs");
 // const { i18n } = require("./next-i18next.config");
 
 const isProd = process.env.NODE_ENV === "production";
+
+/** @type { import("@sentry/nextjs/src/config/types").ExportedNextConfig }*/
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
 
 // If such a type existed...
 /** @type { import("next").NextConfig } */
@@ -16,4 +30,14 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins([], nextConfig);
+module.exports = withPlugins(
+  [
+    [
+      // Make sure adding Sentry options is the last code to run before exporting, to
+      // ensure that your source maps include changes from all other Webpack plugins
+      withSentryConfig,
+      sentryWebpackPluginOptions,
+    ],
+  ],
+  nextConfig
+);
