906b4e09b00e43e2c7b5c543cfafb2afa3645709
Ali Heydari <ali4heydari@gmail.com>
2021-11-29 16:41:40 +0330

    build(deps): 📦 add tailwindcss types package
    add tailwind types to tailwind config to infer propeties
    
    fix #8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   purge: ["./pages/**/*.tsx", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
